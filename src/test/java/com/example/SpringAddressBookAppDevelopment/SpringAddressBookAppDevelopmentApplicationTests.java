8dc6fc8f5dac9f8c43823f71d516b9844a2e09c5
“Sachin” <sachinkumar30690@gmail.com>
2025-03-07 11:54:10 +0530

    main

---
+++ src/test/java/com/example/SpringAddressBookAppDevelopment/SpringAddressBookAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAddressBookAppDevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAddressBookAppDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
